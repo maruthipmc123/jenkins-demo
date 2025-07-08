e8f4a2dcfd21c9ebc1796f0d5467aa210bc95f9e
marushammi <marushammijun10@gmail.com>
2025-07-08 10:46:49 +0530

    rectified some mistakes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js
 const funcs = {
       add: (num1, num2) => {
-		if (typeof num1 !== number' || typeof num2 !== 'number') {
+		if (typeof num1 !== 'number' || typeof num2 !== 'number') {
 			return null;
 		}
 		return num1 + num2;
--- app.test.js
+++ app.test.js
@@ -1,12 +1,11 @@
 // app.test.js
-const funcs = require('./app');
+const funcs=require('./app');
 
 test('adds 2 + 3 to equal 5', () => {
-	expect(funcs.add(2, 3)).toBe(5);
+  expect(funcs.add(2, 3)).toBe(5);
 });
 
 test('should return null if arguments are not numbers', () => {
-	expect(funcs.add(2,'a')).toBeNull();
-
+  expect(funcs.add(2,'a')).toBeNull();
 });
 
